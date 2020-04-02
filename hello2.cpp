34149b6380e877812ebbc0b6c524ca2e1f66e189
Eddie <mahamaek@gmail.com>
2020-04-02 11:20:25 -0400

    initial commit

---
+++ hello2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        //cout << "Hello World 1!!!" << flush;
+        cout << "Hello World !!!" << endl;
+        cout << "FName"
+             << ":"
+             << " "
+             << "Edward" << endl;
+    }
+    return 0;
+}
