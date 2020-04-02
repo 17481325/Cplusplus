04cb80070e8ce0c3cb762d86e2a6b8852c63e4d6
Eddie <mahamaek@gmail.com>
2020-04-02 13:13:40 -0400

    initial commit

---
+++ variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numberCat = 5;
+    int age = 33;
+    string name = "Edward";
+    float balance = 1234.5;
+    cout << "Hello, my name is : "
+         << name << endl;
+    cout << "I am " << age << endl;
+    cout << "I have " << numberCat << "cats" << endl;
+    cout << "I have " << balance << " in my account" << endl;
+    return 0;
+}
