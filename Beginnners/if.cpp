a27425f7efc5afa609bc93b26e5f09e372fa8c7c
Eddie <mahamaek@gmail.com>
2020-04-02 22:31:04 -0400

    initial

---
+++ Beginnners/if.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string storedPassword = "Hello";
+
+    cout << "Enter your password > " << flush;
+    string password;
+    cin >> password;
+
+    if (storedPassword == password)
+    {
+        cout << "access granted" << endl;
+    }
+    else
+    {
+        cout << "access denied" << endl;
+    }
+
+    //cout << "'" << password << "'" << endl;
+    /* code */
+    return 0;
+}
