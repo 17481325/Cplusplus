8ae3630f221e0a09fa3ca7cfcbd41fa30774b8b9
Eddie <mahamaek@gmail.com>
2020-04-03 00:10:00 -0400

    initial commit

---
+++ Beginnners/while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int count = 0;
+    while (count < 5)
+    {
+        cout << "Hello,World" << endl;
+        count++;
+    }
+    return 0;
+}
