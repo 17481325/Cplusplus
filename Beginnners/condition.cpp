87cc8c7e092241ffec3b3aa599c4a7d879dc9b25
Eddie <mahamaek@gmail.com>
2020-04-02 23:36:08 -0400

    initial

---
+++ Beginnners/condition.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int val1 = 2;
+    int val2 = 4;
+
+    cout << "Enter two values: " << flush;
+
+    int input1;
+    int input2;
+
+    cin >> input1;
+    cin >> input2;
+
+    if ((input1 == 2) && (input2 == 4))
+    {
+        cout << "You got both right" << endl;
+    }
+    else if ((input1 == 2) || (input2 == 4))
+    {
+        cout << "You got one right" << endl;
+    }
+    else
+    {
+        cout << "You got all wrong" << endl;
+    }
+
+    return 0;
+}
