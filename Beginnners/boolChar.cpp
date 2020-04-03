4e8353aa5735380fd2e89d26809ad3972ea72088
Eddie <mahamaek@gmail.com>
2020-04-02 22:08:08 -0400

    file reorganization

---
+++ Beginnners/boolChar.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    // bool bval = 1;
+    // bool bval2 = true;
+    // bool bval3 = 0;
+    // bool bval4 = false;
+    // cout << bval << endl;
+    // cout << bval2 << endl;
+    // cout << bval3 << endl;
+    // cout << bval4 << endl;
+
+    // Chars
+
+    char c = '7';
+    char cval = 55;
+    wchar_t w_c = 'i';
+    cout << c << endl;
+    cout << cval << endl;
+    cout << (int)c << endl;
+    cout << (int)'A' << endl;
+    cout << (int)'a' << endl;
+
+    cout << (int)w_c << endl;
+    cout << (char)w_c << endl;
+
+    return 0;
+}
